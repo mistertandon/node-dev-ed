df8939ee8d4f00475344104dfee07201d288af7f
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-08 12:30:27 +0530

    Installed mongodb node.js driver. Installed nodemon to detect changes automatically and reload the server. Developed code to connect with mongo-database using MongoClient provided by mongodb node.js driver.

---
+++ README.md
@@ -0,0 +1,4 @@
+# RestFul API using node.js and mogodb
+
+_ How to excute local npm module _
+$(npm bin)/nodemon
--- app.js
+++ app.js
@@ -12,13 +12,13 @@ var users = require('./routes/users');
 var app = express();
 
 app.engine('.hbs', eHbs({
-        extname        : '.hbs',
-        layoutsDir     : path.join(__dirname, '/views/layouts'),
-        partialsDir    : [
-                            path.join(__dirname, '/views/partials')
-                        ],
-        defaultLayout  : 'main'
-    }));
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, '/views/layouts'),
+  partialsDir: [
+    path.join(__dirname, '/views/partials')
+  ],
+  defaultLayout: 'main'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,14 +36,14 @@ app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
