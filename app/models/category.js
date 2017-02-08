22c7d91c44f8a6df849eb5db3800d5b3c0ea936c
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-08 23:27:35 +0530

    Developed mongodbm model to share mongo database instance among all models into application.

--- app/models/category.js
+++ app/models/category.js
@@ -1,3 +1,6 @@
+var MongoDbI = require('./mongodbm').MongoDbI;
+
+/*
 var mongo = require('mongodb'),
   mongoServer = mongo.Server,
   mongoDB = mongo.Db;
@@ -15,15 +18,23 @@ db.open(function (err, db) {
   }
 
 });
-
+*/
 module.exports.getCategories = function (req, res, next) {
 
-  db.collection('categories', function (err, collection) {
+  /**
+   * Using databse instance we will access categories table.
+   */
+  MongoDbI.collection('categories', function (err, collection) {
 
     if (err !== null) {
       next('Something wrong happend with collection while retrieving documents.');
     }
 
+    /**
+     * `collection` variable having access to categories collection,
+     * using it we can retrieve all documents exist into it.
+     * 
+     */
     collection.find().toArray(function (err, categories) {
 
       if (err !== null) {
