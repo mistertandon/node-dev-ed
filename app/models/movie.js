2df334a71f6fa5566d85fc908753ca3429dec960
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-10 12:25:40 +0530

    Developed mongodb_conf.js file, contains configuration value for mongodb. Developed code to demonstrate colection.find with single field filter.

--- app/models/movie.js
+++ app/models/movie.js
@@ -1,27 +1,57 @@
 var MongoDb_MM = require('./mongodbm'),
   MongoDbI_MM = MongoDb_MM.MongoDbI;
 
-var MongoDb_L = require('./../locale/mongodb_locale').MongoDB_Locale;
+var MongoDb_ML = require('./../locale/mongodb_locale').MongoDb_MLocale;
+var MongoDb_MC = require('./../config/mongodb_cong').MongoDB_CONF;
+
 
 /**
  * This module is used to retrieving documents using `collection.findOne`
  * method. In this case we will use single field as filter.
+ */
+exports.findOneWithOneField = function (req, res, next) {
+
+  MongoDbI_MM.collection('movies', { strict: true }, function (err, collection) {
+
+    if (err !== null) {
+      res.status(500).send({ 'error': MongoDb_ML.CollectionCouldNotAccess });
+    }
+
+    collection.findOne({ rated: 'PG-13' }, { limit: MongoDb_MC.defaultFindLimitOptionValue }, function (err, result) {
+
+      if (err !== null) {
+        res.status(500).send({ 'error': MongoDb_ML.DocumentsCouldNotAccess });
+      }
+
+      res.send(result);
+    });
+
+  });
+}
+
+/**
+ * This module is used to retrieving documents using `collection.findOne`
+ * method. In this case we will use :
+ *
+ * single field for filtering
+ * limit method of find cursor
+ * toArray method of find curosr
  */
 exports.findWithOneField = function (req, res, next) {
 
   MongoDbI_MM.collection('movies', { strict: true }, function (err, collection) {
 
     if (err !== null) {
-      res.status(500).send(JSON.stringify({ 'error': MongoDb_L.CollectionCouldNotAccess }));
+      res.status(500).send({ 'error': MongoDb_ML.CollectionCouldNotAccess });
     }
 
-    collection.findOne({ rated: 'G' }, { limit: 1 }, function (err, result) {
+    collection.find().limit(MongoDb_MC.defaultFindLimitOptionValue).toArray(function (err, documents) {
 
       if (err !== null) {
-        res.status(500).send(JSON.stringify({ 'error': MongoDb_L.DocumentsCouldNotAccess }));
+        res.status(500).send({ 'error': MongoDb_ML.DocumentsCouldNotAccess });
       }
 
-      res.send(result);
+      res.send(documents);
     });
 
   });
--- routes/movies.js
+++ routes/movies.js
@@ -9,7 +9,19 @@ var Movie_MR = require('./../app/models/movie');
  * This route definition handles request for demonstrating retrieving documents using `collection.findOne`
  * method. In this case we will use single field as filter.
  */
-router.get('/findWithOneField', function (req, res, next) {
+router.get('/find-one/with-one-field', function (req, res, next) {
+  Movie_MR.findOneWithOneField(req, res, next);
+});
+
+/**
+ * This route definition handles request for demonstrating retrieving documents using `collection.find`
+ * method. In this case we will use :
+ * 
+ * single field for filtering
+ * limit method of find cursor
+ * toArray method of find curosr
+ */
+router.get('/find/with-one-field/limit/to-array', function (req, res, next) {
   Movie_MR.findWithOneField(req, res, next);
 });
 
