e5f63d06afc366e9ad88bfc11e288560263608b0
mistertandon <enggparveshtandon@gmail.com>
2020-11-15 18:40:50 +0530

    feat: Setup cloud mongodb ATLAS
    1. Create cluster using mongodb ATLAS client.
    2. Create user with write, read roles.
    3. Connect with cloud mongodb mongoose in node app

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express = require(express);
+const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
+mongoose.connect('mongodb+srv://mistertandon:0291noJI**@devednode.gggju.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    { useUnifiedTopology: true },
+    () => {
+
+        console.log('connected to cloud Mongodb');
+    })
+
+/**
+ * Import routes to the application.
+ */
 const appRoute = require('./routes/auth');
+
+app.use('/api/user/', appRoute);
 
 app.listen(
     3000,
