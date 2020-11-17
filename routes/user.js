ab4d2cc0aede893b8f8fe99d2f79ec6114f27e25
mistertandon <enggparveshtandon@gmail.com>
2020-11-18 00:20:33 +0530

    feat: Develop user/profile end point to retrieve profile.

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
+const User = require('./../model/User');
 
 const { verifyJwt } = require('./../token/verifyJwt');
 
 router.get('/profile', verifyJwt, (req, res) => {
 
-    console.log('/profile');
-    res.json({ title: 'secret' });
+    const id = req.user._id;
 
+    User.findById(id, (err, userRecord) => {
+
+        if (err) {
+
+            res.status(401).send('Unknown user');
+        } else {
+
+            res.send(JSON.stringify(userRecord));
+        }
+
+    });
 });
 
 module.exports = router;
